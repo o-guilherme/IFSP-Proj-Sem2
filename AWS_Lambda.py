b4c05dbadae41cc8384fd56924191c0e59bef546
o-guilherme <guilhermecdainese@gmail.com>
2022-11-21 15:28:32 -0300

    update

--- AWS_Lambda.py
+++ AWS_Lambda.py
@@ -1,11 +1,16 @@
-!pip install s3fs==2022.01.0
-from  joblib import load, dump
-import pandas as pd
-loaded_forest_model = load('s3://trabalho-ifsp-campinas-interdisciplinar-2022-2/random_forest_model.pkl')
-test_set = pd.read_csv('s3://trabalho-ifsp-campinas-interdisciplinar-2022-2/test_data.csv')
+import s3fs
+import joblib
 
-X = dataset.drop(columns='Response')
+model_open = open('s3://trabalho-ifsp-campinas-interdisciplinar-2022-2/random_forest_model.pkl', 'rb')
+model_open.close()
 
-prediction = loaded_forest_model.predict(X)
+print('ok')
 
-joblib.dump(prediction,'s3://trabalho-ifsp-campinas-interdisciplinar-2022-2/prediction.txt')
+model = open('s3://trabalho-ifsp-campinas-interdisciplinar-2022-2/random_forest_model.pkl', 'b')
+loaded_forest_model = joblib.loadmodel)
+
+loaded_data = joblib.load('s3://trabalho-ifsp-campinas-interdisciplinar-2022-2/test_data_lambda.pkl')
+
+prediction = loaded_forest_model.predict(loaded_data)
+
+joblib.dump(prediction,'s3://trabalho-ifsp-campinas-interdisciplinar-2022-2/prediction.pkl')
